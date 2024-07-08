409925c0c88070db14005b94ab339e3bddc13241
mjasir-10 <jasirmm307@gmail.com>
2024-07-08 14:16:55 +0600

    section layout added

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+
+export  {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Services,
+    Footer,
+    Subscribe,
+    SuperQuality,
+    SpecialOffer
+}
